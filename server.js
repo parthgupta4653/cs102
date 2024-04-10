efdff906751b8fa8c0d54816f76aa22c6a21b1fc
parthgupta4653 <rayanscammander@gmail.com>
2024-04-11 00:17:22 +0530

    test commit

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const xlsx = require('xlsx')
 const bcrypt = require("bcrypt")
 
 const all_subjects_val = {"MA102":"none", "CS102":"none", "NO101":"none", "EE101":"none"}
+let attendanceCode = 0;
+var attendanceArray = []
 
-const db = mysql.createConnection({host:"127.0.0.1", user:"root", password:"root", database:"cs102" })
+const db = mysql.createConnection({host:"127.0.0.1", user:"root", password:"", database:"cs102" })
 db.connect((error) =>{
     if (error){throw error}
 })
 
-
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-
 
 passport.use(new LocalStrategy({ usernameField: 'email' }, function(email,password,done){
   db.query("select * from users where email='"+email+"' ",async function(err,results){
@@ -66,11 +66,29 @@ app.use(express.json())
 
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
+app.get("/s_ma102", (req,res)=>{
+  db.query("select * from posts where subject='MA102'", function(err,results){
+    results =  Object.values(JSON.parse(JSON.stringify(results)))
+    //console.log(results)
+    res.render("post", {messages: results})
+  })
+  
+})
+
 app.get("/login", checkNotAuthenticated, (req,res)=>{
   res.render("login")
 })
 
-app.get("/", checkAuthenticated,(req, res)=>{
+app.get("/", checkAuthenticated, (req,res)=>{
+  if(req.user.type==="student"){
+    res.redirect("/student")
+  }
+  else{
+    res.redirect("/teacher")
+  }
+})
+
+app.get("/student", (req, res)=>{
   let subjects = req.user.subjects.split(" ")
   all_subjects = Object.keys(all_subjects_val)
 
@@ -86,8 +104,24 @@ app.get("/", checkAuthenticated,(req, res)=>{
   }
 })
 
-app.get("/profile", checkAuthenticated, (req,res)=>{
+app.get("/profile", (req,res)=>{
   res.render("profile", {name: req.user.name, id:req.user.id, email:req.user.email, branch: req.user.branch, batch: req.user.batch})
+})
+
+app.get("/teacher", checkAuthenticated, (req, res)=>{
+  let subjects = req.user.subjects.split(" ")
+  all_subjects = Object.keys(all_subjects_val)
+
+  for(let i=0; i<all_subjects.length; i++){
+    if(subjects.includes(all_subjects[i])){
+      all_subjects_val[all_subjects[i]] = "block";
+    }
+  }
+
+  res.render("home_page", {ma102:all_subjects_val['MA102'], cs102:all_subjects_val['CS102'], ee101:all_subjects_val['EE101'], no101:all_subjects_val['NO101']})
+  for (let key in all_subjects_val) {
+     all_subjects_val[key] = "none"
+  }
 })
 
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -136,6 +170,100 @@ app.post("/downloadattendance_no101", function(req,res){
 
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
+app.get("/editattendance_ma102", function(req,res){
+  subject="MA102" 
+  id=1
+  date="2024-04-09"
+  stat="P"
+  update_attendance(id,subject,stat,date)
+  res.redirect("/teacher")
+})
+
+app.get("/editattendance_cs102", function(req,res){
+  subject="CS102" 
+  id=1
+  date="2024-04-09"
+  stat="P"
+  update_attendance(id,subject,stat,date)
+  res.redirect("/teacher")
+})
+
+app.get("/editattendance_ee101", function(req,res){
+  subject="EE101" 
+  id=1
+  date="2004-04-09"
+  stat="P"
+  update_attendance(id,subject,stat,date)
+  res.redirect("/teacher")
+})
+
+app.get("/editattendance_no101", function(req,res){
+  subject="NO101" 
+  id=1
+  date="2024-04-09"
+  stat="P"
+  update_attendance(id,subject,stat,date)
+  res.redirect("/teacher")
+})
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+app.post("/post_ma102", function(req,res){
+  text = req.body.message
+  //console.log(text)
+  var timestamp = new Date().toString().slice(0,21)
+  db.query("INSERT INTO posts (message, sender, subject, time) VALUES ('"+text+"', '"+req.user.name+"', 'MA102', '"+timestamp+"')")
+  //console.log(timestamp)
+  res.redirect("/s_ma102")
+})
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+app.post("/take_attendance_ma102", function(req,res){
+  let query = "INSERT INTO attendance (id, subject, date, status) VALUES"
+  db.query("SELECT id FROM users WHERE type = 'student' and subjects LIKE '%MA102%'",function (err,results){
+    if(err){throw err}
+    results =  Object.values(JSON.parse(JSON.stringify(results)))
+    date = new Date().toJSON().slice(0,10)
+    for(i in results){
+      query += "('"+results[i].id+"', 'MA102', '"+date+"', 'A'), "
+    }
+    query = query.substring(0, query.length - 2);
+    db.query(query,(err)=>{
+      if(err){console.log(err)}
+    })
+
+    attendanceCode = Math.floor(1000 + Math.random() * 9000);
+    
+    setTimeout(()=>{
+      console.log("t:")
+      console.log(attendanceArray)
+      for (i in attendanceArray){
+        db.query("UPDATE attendance SET status = 'P' WHERE id = '"+attendanceArray[i]+"'AND subject = 'MA102' AND date = '"+date+"'")
+      }
+      attendanceCode = 0
+      attendanceArray = []
+      console.log("Done")
+      res.redirect("/student")
+    },40000)
+    console.log(attendanceCode)
+  })
+})
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+app.post("/set_attendance_ma102", (req,res)=>{
+  let code = req.body.password
+  console.log(code)
+  if(code == attendanceCode && attendanceCode !==0){
+    attendanceArray.push(req.user.id)
+  }
+  console.log("S:")
+  console.log(attendanceArray)
+  res.redirect("/student")
+})
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return next()
@@ -144,14 +272,16 @@ function checkAuthenticated(req, res, next) {
   
     res.redirect('/login')
   }
+
   function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
       return res.redirect('/')
     }
     next()
   }
+
   function get_attendance(id, sub){
-    db.query("select * from attendance where id="+id+" and subject= '"+sub+"'", function(err, results){
+    db.query("select date,status from attendance where id="+id+" and subject= '"+sub+"'", function(err, results){
       if(err){throw err;}
       results = Object.values(JSON.parse(JSON.stringify(results)))
       for (i in results){results[i].date=results[i].date.substring(0,10)}
@@ -162,5 +292,14 @@ function checkAuthenticated(req, res, next) {
       xlsx.writeFile(book, "attendance.xlsx")   
     });
   }
+  function update_attendance(id,subject,stat,date){
+    db.query("UPDATE attendance set status='"+stat+"' where subject='"+subject+"' AND id="+id+" AND date= '"+date+"'",async function(err,results){
+      if(err){throw err}
+      //results = Object.values(JSON.parse(JSON.stringify(results)))
+      console.log(results)
+      if(results.affectedRows===0){console.log("ewfrg")}
+    })
+  }
+  
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 app.listen(3000)
